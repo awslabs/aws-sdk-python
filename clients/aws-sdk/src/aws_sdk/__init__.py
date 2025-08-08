b1ab7f9cefc1c63677fb820b06c6f68abeb0fc46
jonathan343 <jonathangee09@gmail.com>
2025-08-08 13:38:00 -0400

    Hardcode version in __init__.py and set it dynamically in pyproject.toml

--- clients/aws-sdk/src/aws_sdk/__init__.py
+++ clients/aws-sdk/src/aws_sdk/__init__.py
@@ -1,7 +1,6 @@
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
-import importlib.metadata
+__version__ = "0.0.1"
 
-__version__: str = importlib.metadata.version(__name__)
 
 # TODO: Consider adding relative imports for services from the top level namespace?
