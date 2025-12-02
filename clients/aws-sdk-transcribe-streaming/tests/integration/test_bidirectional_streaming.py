915eb5bb8817af5853f3eeb98f885662ac6919a3
Alessandra Romero <24320222+alexgromero@users.noreply.github.com>
2025-12-02 16:18:41 -0500

    Update clients/aws-sdk-transcribe-streaming/tests/integration/test_bidirectional_streaming.py
    Co-authored-by: Nate Prewitt <nate.prewitt@gmail.com>

--- clients/aws-sdk-transcribe-streaming/tests/integration/test_bidirectional_streaming.py
+++ clients/aws-sdk-transcribe-streaming/tests/integration/test_bidirectional_streaming.py
@@ -1,7 +1,7 @@
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 
-"""Test bidirectional streaming duplex event stream handling."""
+"""Test bidirectional event stream handling."""
 
 import asyncio
 import time
