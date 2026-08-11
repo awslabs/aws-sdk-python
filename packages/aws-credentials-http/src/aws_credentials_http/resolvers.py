e0b561fb74829d65e82f26bdc6f5a739a3ef3564
Antonio Aranda <102337110+arandito@users.noreply.github.com>
2026-08-11 17:25:31 -0400

    Address feedback

--- packages/aws-credentials-http/src/aws_credentials_http/resolvers.py
+++ packages/aws-credentials-http/src/aws_credentials_http/resolvers.py
@@ -95,6 +95,7 @@ class ContainerCredentialsResolver(
                 host=parsed.hostname or "",
                 port=parsed.port,
                 path=parsed.path,
+                query=parsed.query,
             )
         else:
             raise SmithyIdentityError(
--- packages/aws-credentials-imds/src/aws_credentials_imds/__init__.py
+++ packages/aws-credentials-imds/src/aws_credentials_imds/__init__.py
@@ -2,12 +2,13 @@
 # SPDX-License-Identifier: Apache-2.0
 __version__ = "0.0.0"
 
-from .client import IMDSConfigurationError
+from .client import IMDSConfig, IMDSConfigurationError
 from .providers import Ec2InstanceMetadataProvider
 from .resolvers import IMDSCredentialsResolver
 
 __all__ = (
     "Ec2InstanceMetadataProvider",
+    "IMDSConfig",
     "IMDSConfigurationError",
     "IMDSCredentialsResolver",
 )
